13612979f6dd29b6accfa39e2902836c34d58ee4
Gagandeep Singh Choudhary <gchoudhary16555@gmail.com>
2025-08-24 17:48:43 +0530

    Create partition-array-into-equal-sum-subarrays.cpp

---
+++ Leetcode/Arrays/partition-array-into-equal-sum-subarrays.cpp
@@ -0,0 +1,48 @@
+/*
+LeetCode Problem: 3659. Partition Array Into Equal Sum Subarrays
+Problem Link: https://leetcode.com/problems/partition-array-into-equal-sum-subarrays/
+
+Difficulty: Medium
+Tags: Array, Hashing, Sorting, Greedy
+
+Problem Summary:
+You are given an integer array `nums` and an integer `k`. 
+Return true if it is possible to divide `nums` into some number of subarrays such that:
+- Each subarray has exactly `k` elements.
+- All subarrays have the same sum.
+Otherwise, return false.
+
+Example:
+Input: nums = [1,2,2,1], k = 2
+Output: true
+Explanation: We can divide nums into [1,2] and [2,1] with equal sum (3).
+
+Approach:
+- First check if nums.size() is divisible by k; if not, return false.
+- Calculate `cnt = nums.size()/k`, the number of subarrays.
+- Count the frequency of each element using a map.
+- If any element occurs more than `cnt` times, return false because we cannot distribute them evenly.
+- Otherwise, return true.
+
+Time Complexity: O(n log n) [due to map]
+Space Complexity: O(n)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        if (nums.size() % k != 0) return false;
+        int cnt = nums.size() / k;
+        map<int,int> mp;
+        for (auto &e : nums) {
+            mp[e]++;
+        }
+        for (auto &it : mp) {
+            if (it.second > cnt) return false;
+        }
+        return true;
+    }
+};
